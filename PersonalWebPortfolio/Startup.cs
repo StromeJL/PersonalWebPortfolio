11a6199989abead3ef3f6da8ad5ccad38eaf49d8
StromeJL <stromejl@gmail.com>
2016-08-12 14:39:27 -0400

    Add project files.

---
+++ PersonalWebPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalWebPortfolio.Startup))]
+namespace PersonalWebPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
